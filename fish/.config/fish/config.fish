7cecb7252832f8c01b70fbe4208b00e3a878d459
sele01 <sele.edu21@gmail.com>
2026-07-09 15:42:33 +0300

    some updates and console api ai usage addition

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,7 @@
 # ==========================================
 # BASH CONFIGURATION BRIDGES (SHARED FILES)
 # ==========================================
+fish_add_path ~/.local/bin
 
 # 1. Load the shared typing environment safely
 if test -f ~/.typing
@@ -14,3 +15,9 @@ end
 
 # Initialize Zoxide for Fish
 zoxide init fish | source
+
+# Created by `pipx` on 2026-07-09 07:23:47
+set PATH $PATH /home/skymoon/.local/bin
+
+#API from openroute
+set -gx OPENROUTER_API_KEY  "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"
